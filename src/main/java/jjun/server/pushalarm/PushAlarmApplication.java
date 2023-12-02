dda169472e81160128f1c230e89df6b18a62cc3c
jun02160 <jun020216@naver.com>
2023-12-03 05:13:29 +0900

    [INIT] 프로젝트 초기 세팅

---
+++ src/main/java/jjun/server/pushalarm/PushAlarmApplication.java
@@ -0,0 +1,13 @@
+package jjun.server.pushalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushAlarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jjun/server/pushalarm/PushAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package jjun.server.pushalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
