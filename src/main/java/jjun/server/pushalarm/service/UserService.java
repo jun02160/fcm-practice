025c9f7f336e2aaf6427b2e4ef482d0d1a0684e5
jun02160 <jun020216@naver.com>
2023-12-03 13:42:43 +0900

    [FEAT] User API 호출 시 푸시메시지 전송하도록 추가

--- src/main/java/jjun/server/pushalarm/service/UserService.java
+++ src/main/java/jjun/server/pushalarm/service/UserService.java
@@ -1,5 +1,9 @@
 package jjun.server.pushalarm.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import java.io.IOException;
+import jjun.server.pushalarm.domain.User;
+import jjun.server.pushalarm.dto.fcm.FCMPushRequestDto;
 import jjun.server.pushalarm.dto.response.GetUserResponse;
 import jjun.server.pushalarm.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +17,15 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    // 푸시알림 활성화가 필요한 로직이라면, FCMService를 주입!
+    private final FCMService fcmService;
+
     public GetUserResponse getUserById(Long userId) {
-        return GetUserResponse.of(userRepository.findById(userId).get());
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new EntityNotFoundException("존재하지 않는 회원의 아이디입니다.")
+        );
+        //== User 조회 API 호출 시 푸시 알림 전송! ==//
+        fcmService.pushAlarm(FCMPushRequestDto.sendTestPush(user.getFcmToken()));
+        return GetUserResponse.of(user);
     }
 }
